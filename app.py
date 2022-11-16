8a2f3576d1d4df98d011a2522050e77a6aefebc3
farrel216 <farrel.arp@gmail.com>
2022-11-17 01:37:21 +0700

    form index and routing

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import flask
 from flask import Flask,redirect, url_for, request, render_template
+import pickle
 app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
 @app.route('/')
 @app.route('/index')
 def index():
@@ -8,4 +10,9 @@ def index():
 
 @app.route('/hasil' , methods = ['POST', 'GET'])
 def hasil():
-    return render_template('hasil.html')
+    if request.method == 'POST':
+        nama = request.form['name']
+        sunshine = request.form['sunshine-hour']
+        work = request.form['work-hour']
+        result = "Hello " + nama + " , " + sunshine + " hours of sunshine per day and " + work + " hours of work per day will make you happy."
+        return render_template("index.html",result=result)
